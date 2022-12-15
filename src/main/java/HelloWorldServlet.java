18762f36456185f68bdc89a0b52c1350267d1e65
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-12-15 11:20:06 -0500

    Servlets exercises completed

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,12 +6,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
+    private int count = 0;
+
+//    public void init(){
+//        count = 0;
+//    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
         response.setContentType("text/html");
+        count++;
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Hello, World!</h1>" + "<h2>Number of times page viewed: " + count +"</h2>" + "Hello, " + request.getParameter( "name")+ "!");
+
     }
 }
