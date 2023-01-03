1b01b0fc63aba99d47bca01fff2791a8ce5cb6ff
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2023-01-03 10:58:18 -0500

    Accessing Data exercises completed

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        req.getRequestDispatcher("/login.jsp").forward(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            res.sendRedirect("/profile");
+        } else {
+            res.sendRedirect("/login");
+        }
+    }
+}
+
+
+
